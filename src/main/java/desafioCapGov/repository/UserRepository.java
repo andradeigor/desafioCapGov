fd8a999a1a06da157584f8070f8f5c42b4df8660
Igor <ig.vp@hotmail.com>
2023-04-10 20:12:52 -0300

    post for user done

--- src/main/java/desafioCapGov/repository/UserRepository.java
+++ src/main/java/desafioCapGov/repository/UserRepository.java
@@ -2,10 +2,13 @@ package desafioCapGov.repository;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.hibernate.Session;
 
@@ -17,12 +20,25 @@ public class UserRepository {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<User> getPessoas() {
+	public Response getPessoas() {
 		Session session = Database.getSession();
 		List<User> users = session.createQuery("from User").list();
 		session.close();
 		users.forEach(user -> user.setPassword(null));
-		return users;
+		return Response.ok().entity(users).build();
 	}
 
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createUser(User u) {
+		User user = new User(u.getName(), u.getEmail(), u.getPassword());
+		Session session = Database.getSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+		session.close();
+		user.setPassword(null);
+		return Response.status(201).entity(user).build();
+	}
 }
