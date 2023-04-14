4f5c4b54c113444e800ebc0005aa79477942afe2
Igor <ig.vp@hotmail.com>
2023-04-14 08:39:48 -0300

    finish the post route to posts

--- src/main/java/desafioCapGov/repository/PostRepository.java
+++ src/main/java/desafioCapGov/repository/PostRepository.java
@@ -8,6 +8,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -29,14 +31,15 @@ public class PostRepository {
 		session.beginTransaction();
 		List<Post> posts = session.createQuery("from Post").list();
 		session.getTransaction().commit();
-		System.out.println("to aqui");
 		return Response.ok().entity(posts).build();
 	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response createPost(Post post) {
+	public Response createPost(Post p, @Context HttpHeaders headers) {
+		String userID = headers.getHeaderString("userID");
+		Post post = new Post(p.getTitle(), p.getContent(), userID);
 		Session session = Database.getSession();
 		session.beginTransaction();
 		session.save(post);
